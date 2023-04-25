f90178b3e28b3ce0e131fc12d2ac091d1df46d60
Karol <karolc9991@gmail.com>
2023-04-25 19:34:29 +0200

    Create printer and parse headers from yaml

--- autocall/call.py
+++ autocall/call.py
@@ -2,50 +2,80 @@ import requests
 import yaml
 import json
 import validate
+import printer
 from typing import List
 
 OP_GET = "GET"
 OP_POST = "POST"
+OP_PUT = "PUT"
+OP_DELETE = "DELETE"
+
+DEFAULT_TIMEOUT = 300
 
 class Call:
-    def __init__(self, call_id, url, op, expect, headers = None, body = None):
+    def __init__(self, call_id, url, op, expect, headers = None, body = None, timeout = DEFAULT_TIMEOUT):
         self.call_id = call_id
         self.url = url
         self.op = op
         self.expect = expect
         self.headers = headers
-        if body:
-            self.body = json.loads(body)
+        self.body = body
+        self.timeout = timeout
 
-    def execute(self):
+    def execute(self, print_to_console = True):
         res = None
+        if self.body:
+            self.body = json.loads(self.body)
+
         if self.op == OP_GET:
-            res = requests.get(self.url)
-        if self.op == OP_POST:
-            res = requests.post(self.url, json=self.body)
-        print(res.json())
+            res = requests.get(self.url, headers=self.headers, json=self.body, timeout=self.timeout)
+        elif self.op == OP_POST:
+            res = requests.post(self.url, headers=self.headers, json=self.body, timeout=self.timeout)
+        elif self.op == OP_PUT:
+            res = requests.put(self.url, headers=self.headers, json=self.body, timeout=self.timeout)
+        elif self.op == OP_DELETE:
+            res = requests.delete(self.url, headers=self.headers, json=self.body, timeout=self.timeout)
+
+        if print_to_console:
+            printer.print_call(self.expect, self.call_id, res)
 
 with open('config.yml', encoding='utf-8') as f:
     config = yaml.safe_load(f)
 
-calls = []
+
+# FIXME: This is horribly bad
+def parse_headers(call):
+    headers_str : str = "{"
+    for key,value in call['headers'].items():
+        headers_str += f'"{key}" : "{value}",'
+    headers_str = headers_str[:-1]
+    headers_str += "}"
+    headers = json.loads(headers_str)
+    return headers
+
 
 def create_calls() -> List[Call]:
+    calls = []
     for c in config['calls']:
         call = c['call']
-
+        id = call['id']
         url = call['url'] 
         expect = call['expect'] 
         op = call['op']
+        timeout = DEFAULT_TIMEOUT
+
         body = None
         headers = None
-
+        
         if 'body' in call:
             body = call['body']
         if 'headers' in call:
-            headers = call['headers']
+            headers = parse_headers(call)
+        if 'timeout' in call:
+            timeout = call['timeout']
+
         if validate.validate_call(call):
-            calls.append(Call(0, url, op, expect, headers, body))
+            calls.append(Call(id, url, op, expect, headers, body, timeout))
         else:
             print(f'Error validating call inside yaml file: {call}')
     return calls
--- autocall/validate.py
+++ autocall/validate.py
@@ -1,22 +1,22 @@
-import validators
 import json
+import validators
 
 OPS = ("POST", "GET", "PUT", "DELETE") 
 
 def validate_call(call):
     try:
         url = call['url']
-        assert(validators.url(url))
+        assert validators.url(url)
 
         expect = call['expect']
-        assert(int(expect))
+        assert int(expect)
         
         op = call['op']
-        assert(op in OPS)
+        assert op in OPS
 
         if 'body' in call:
             body = call['body']
-            assert(json.loads(body))
+            assert json.loads(body)
 
     except KeyError:
         print('Unable to parse yaml')
