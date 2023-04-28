21d1fbce3a5e8feb7662719c0e480b3e40cf1fd1
karol <karolc9991@gmail.com>
2023-04-28 20:23:01 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='autocall',
+      version='0.1',
+      description='Automatize network calls through yaml definitions',
+      author='Karol Ciechlecki',
+      author_email='karolc9991@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['autocall'],
+     )
