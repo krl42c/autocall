587154d9524f583f2e68f274397a83e8ef23ad37
Karol <karolc9991@gmail.com>
2023-04-30 12:14:58 +0200

    add custom exceptions to validator

--- autocall/validator.py
+++ autocall/validator.py
@@ -2,16 +2,29 @@ import json
 import validators
 from . import constants
 
+valid_top_level_keys = (
+    'id', 
+    'url', 
+    'expect', 
+    'method',
+    'body', 
+    'tests', 
+    'headers', 
+    'timeout'
+)
+
 def validate_call(call):
     try:
         url = call['url']
-        assert validators.url(url)
+        if not validators.url(url):
+            raise ACMalformedUrlException()
 
         expect = call['expect']
-        assert int(expect)
+        assert int(expect) #TODO: Check that expect is a valid HTTP code
         
         op = call['method']
-        assert op in constants.METHODS
+        if op not in constants.METHODS:
+            raise ACBadHTTPMethod(f"Unrecognized HTTP method {op}")
 
         if 'body' in call:
             body = call['body']
@@ -21,8 +34,25 @@ def validate_call(call):
             assert 'body' in call['tests'][0]
             for c in call['tests']:
                 assert json.loads(c['body'])
+        
+        for key in call.keys():
+            if key not in valid_top_level_keys:
+                raise ACUnrecognizedFieldException(f"Unrecognized field {key}")
+
 
     except KeyError:
-        print('Unable to parse yaml')
+        print('Unable to parse call')
         return False
     return True
+
+class ACMalformedUrlException(Exception):
+    def __init__(self):
+        super().__init__("Malformed URL")
+
+class ACUnrecognizedFieldException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class ACBadHTTPMethod(Exception):
+    def __init__(self, message):
+        super().__init__(message)
