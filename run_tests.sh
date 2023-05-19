ecad66089315394b6159d385434ea6b57b07c33a
Karol <karolc9991@gmail.com>
2023-05-19 19:58:17 +0200

    redirect server output on run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,10 @@ if [[ -z "$INSTALL" ]]; then
     pip3 install -e .
 fi
 cd tests/test_server/
-./start.sh  &
+./start.sh > stdout 2>&1 & 
 cd ../../
 python3 -m pytest tests
 
 # clean
 kill $(lsof -t -i:8000)
-
+echo Tests completed
