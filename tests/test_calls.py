9acf8c275cde2e2a262d8469af1a1e86207b07ea
Karol <karolc9991@gmail.com>
2023-05-27 20:20:35 +0200

    add request mocks for tests

--- tests/test_calls.py
+++ tests/test_calls.py
@@ -2,15 +2,19 @@ import pytest
 import requests
 import json
 import yaml
-from autocall import autocall,validator
+from typing import List
+from autocall import autocall,validator,constants
 
 MULTIPLE_CALLS = "tests/sets/parametrized.yml"
 ALL_METHODS = "tests/sets/general.yml"
 UNRECOGNIZED = "tests/sets/unrec.yml"
 UNEXCEPTED = "tests/sets/unexcepted.yml"
 
-def test_multiple_calls():
+def test_multiple_calls(requests_mock):
     calls = autocall.create_calls(MULTIPLE_CALLS)
+
+    init_mocks(requests_mock, calls)
+    
     try:
         [val.execute() for key,val in calls.items()]
     except requests.RequestException:
@@ -19,24 +23,33 @@ def test_multiple_calls():
         pytest.fail("JSON decode failed")
 
 
-def test_all_methods():
-    calls = autocall.create_calls(ALL_METHODS)
-    try:
-        [val.execute() for key,val in calls.items()]
-    except requests.RequestException:
-        pytest.fail("Request failed")
-    except json.JSONDecodeError:
-        pytest.fail("JSON decode failed")
+def test_unrecognized_field(requests_mock):
+    calls = autocall.create_calls(MULTIPLE_CALLS)
 
+    init_mocks(requests_mock, calls)
 
-def test_unrecognized_field():
+    for call in calls.values():
+        if call.method == constants.M_GET:
+            #requests_mock.get(url = call.url, headers=call.headers, params=call.params, json=call.body, timeout=call.timeout)
+            requests_mock.get(call.url)
+        if call.method == constants.M_POST:
+            requests_mock.post(call.url)
+        if call.method == constants.M_PUT:
+            requests_mock.put(call.url)
+        if call.method == constants.M_DELETE:
+            requests_mock.delete(call.url)
+
     with pytest.raises(validator.UnrecognizedFieldException):
         for c in load_config_file(UNEXCEPTED)['calls']:
             current_call = c['call']
             validator.validate_call(load_config_file(UNRECOGNIZED))
 
 
-def test_excepted_field_missing():
+def test_excepted_field_missing(requests_mock):
+    calls = autocall.create_calls(MULTIPLE_CALLS)
+
+    init_mocks(requests_mock, calls)
+
     with pytest.raises(validator.ExceptedFieldMissing):
         for c in load_config_file(UNEXCEPTED)['calls']:
             current_call = c['call']
@@ -46,3 +59,16 @@ def test_excepted_field_missing():
 def load_config_file(config):
     with open(config, 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
+    
+
+def init_mocks(requests_mock, calls):
+    for call in calls.values():
+        if call.method == constants.M_GET:
+            requests_mock.get(call.url)
+        if call.method == constants.M_POST:
+            requests_mock.post(call.url)
+        if call.method == constants.M_PUT:
+            requests_mock.put(call.url)
+        if call.method == constants.M_DELETE:
+            requests_mock.delete(call.url)
+    
--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -1,9 +1,12 @@
 import pytest
-from autocall import autocall
+from autocall import autocall, constants
 
 OAUTH_SET = "tests/sets/oauth.yaml"
 
 
-def test_oauth():
+def test_oauth(requests_mock):
     call_set = autocall.create_calls(OAUTH_SET)
+    requests_mock.post('http://localhost:8000/token?client_id=23238IQsdj&client_secret=ksaudioaud12983u2')
+    requests_mock.get('http://localhost:8000')
     [val.execute() for key,val in call_set.items()]
+
