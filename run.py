92d60e435d659a86c5bc181d5a6dcce88be3898f
Karol <karolc9991@gmail.com>
2023-07-01 19:03:05 +0200

    run.py

--- run.py
+++ run.py
@@ -6,5 +6,4 @@ from autocall import autocall, reporter
 if __name__ == '__main__':
     callset = autocall.create_calls(sys.argv[1])
     autocall.execute(callset, save_report=True)
-    reporter.create_html_report(callset.values(), 'my_report.html')
 
