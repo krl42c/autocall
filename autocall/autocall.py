f8af9099ee9c16192c6810f05bd49fce0162e2ca
Karol <karolc9991@gmail.com>
2023-07-07 20:48:53 +0200

    threads!

--- autocall/autocall.py
+++ autocall/autocall.py
@@ -1,11 +1,12 @@
 import yaml
 import os
 import logging
+import time
 from random import randrange
 from typing import List
 from autocall import  validator, printer, constants
 from autocall.call import Call
-from autocall.reporter import EntryBuilder
+from autocall.reporter import EntryBuilder, ReportHelper
 
 class SetHandler:
     exception_map = (
@@ -22,10 +23,12 @@ class SetHandler:
         with open(file_path, encoding='utf-8') as file:
             config = yaml.load(file, Loader=yaml.UnsafeLoader)
         calls = {}
+        call_set_number = 0
         for c in config['calls']:
             call : dict = c['call']
+            default_name = f'REQUEST_SET_{call_set_number}'
+            name = call.get('id', default_name)
 
-            name = call.get('id', str(randrange(0,10000000))) # bug prone?
         # Try to validate current call, if validator throws an exception skip it and continue
             try:
                 validator.validate_call(call)
@@ -40,16 +43,16 @@ class SetHandler:
             expect = call.get('expect', 200)
             method = call.get('method')
 
-            query_params = call.get('params')
-            body = call.get('body')
-            headers = call.get('headers')
-            tests = call.get('tests')
-            timeout = call.get('timeout', constants.DEFAULT_TIMEOUT)
-            oauth = call.get('oauth')
-            dynamic = call.get('dynamic')
+            query_params : dict = call.get('params', None)
+            body : dict = call.get('body', None)
+            headers : dict = call.get('headers', None)
+            tests : dict = call.get('tests', None)
+            timeout : int = call.get('timeout', constants.DEFAULT_TIMEOUT)
+            oauth : dict = call.get('oauth', None)
+            dynamic : bool = call.get('dynamic', False)
 
             calls[name] = Call(name, url, method, expect, headers, query_params, body, timeout, tests, oauth=oauth, dynamic=dynamic)
-
+            call_set_number += 1
         return calls
 
     @staticmethod
@@ -58,13 +61,36 @@ class SetHandler:
         pass
     
     @staticmethod
-    def run_set(call_set : dict[str, Call]):
+    def run_set(call_set : dict[str, Call], output : bool = True, html : bool = True):  # call.execute() for each call of the callset
+        if os.environ.get('THREADS') == '1':
+            from concurrent.futures import ProcessPoolExecutor
+
+            start_time = time.time()
+            pool = ProcessPoolExecutor(max_workers=10)
+            pool.map(Call.execute, call_set.values())
+            end_time = time.time() - start_time
+            [print(EntryBuilder.default(call)) for call in call_set.values()]
+
+            if os.environ.get('DEBUG') == '1':
+                print(f'All threads ({len(call_set.values())} sets) finished in: {end_time}')
+            return
+
+
+        start_time = time.time()
         for _, call in call_set.items():
             call.execute()
-            entry = EntryBuilder.default(call)
-            print(entry)
+        end_time = time.time() - start_time
+        
+        [print(EntryBuilder.default(call)) for call in call_set.values()]
+        if os.environ.get('DEBUG') == '1':
+            print(f'All calls ({len(call_set.values())} sets) finished in: {end_time}')
+
+        if html:
+            html_report = ReportHelper.create_html_report(call_set)
+            with open('report.html', 'w') as f:
+                f.write(html_report)
 
     @staticmethod
-    def run_set_tests(call_set : dict[str, Call]):
+    def run_set_tests(call_set : dict[str, Call]): 
         for _,call in call_set.items():
             call.run_tests()
